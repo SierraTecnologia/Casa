b4fe270381bb143fb24b167abc4c21de19f62f4b
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2020-12-08 05:21:58 -0300

    Renomeando entitys para entities

--- src/Http/Resources/Entities/PhotoPlainResource.php
+++ src/Http/Resources/Entities/PhotoPlainResource.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Casa\Http\Resources\Entitys;
+namespace Casa\Http\Resources\Entities;
 
 use Population\Manipule\Entities\PhotoEntity;
 use Illuminate\Http\Resources\Json\Resource;
@@ -11,7 +11,7 @@ use function SiUtils\Helper\to_string;
 /**
  * Class PhotoPlainResource.
  *
- * @package Casa\Http\Resources\Entitys
+ * @package Casa\Http\Resources\Entities
  */
 class PhotoPlainResource extends Resource
 {
